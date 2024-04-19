d07d3bf821266d6ca947bacbd0af3c0c7e282d3e
Nico Krause <post@nicokrause.com>
2024-04-19 12:30:15 +0500

    generating mnemonic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import "carbon-components-svelte/css/all.css";
+	import { Theme, Button } from 'carbon-components-svelte';
+	import { generateMnemonic } from 'bip39'
+
+	// import { generateMnemonic }  from "doichainjs-lib"
+	let theme = "g90";// "white" | "g10" | "g80" | "g90" | "g100"
+	let mnemonic = ''
+</script>
+<Theme bind:theme persist persistKey="__carbon-theme" />
+
+<h1>Welcome to Doichain Playground</h1>
+<h2>1. Create a Mnemonic</h2>
+
+<Button class="button" on:click={ async () => {
+	mnemonic = generateMnemonic()
+}}>Create Mnemonic</Button>
+
+Mnemonic:{mnemonic}
+
+
+<h2>2. Get Balance of the address</h2>
+<Button class="button">Get Balance</Button>
+<p>Little helper for Doichain MultiSig Transactions</p>
+<!--<TextInput labelText="User name" placeholder="Enter Doichain Address..." />-->
+<!--<Button>Get Balance</Button>-->
+<style>
+    h1,h2 {
+        margin-left: 20px;
+        margin-top: 20px;
+    }
+	:global(.button) {
+        margin-left: 20px;
+        margin-top: 20px;
+    }
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		nodePolyfills({
+		// exclude: ['fs'],
+		globals: {
+			Buffer: true,
+			global: true,
+			process: true
+		},
+		protocolImports: true
+	})],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
