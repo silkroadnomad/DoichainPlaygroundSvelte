ec171888c5fb3b461dcb4e2c487300b7105225d4
Nico Krause <post@nicokrause.com>
2024-04-20 19:31:48 +0500

    derivation of a p2pkh address

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,24 @@
 <script>
-	import { onMount } from 'svelte';
 	import "carbon-components-svelte/css/all.css";
 	import { Theme, Button, TextArea, Grid, Row, Column, TextInput } from 'carbon-components-svelte';
 	import { generateMnemonic, mnemonicToSeedSync } from 'bip39'
-
+	import {payments} from 'bitcoinjs-lib';
+	import * as ecc from 'tiny-secp256k1';
 	import BIP32Factory from 'bip32';
-	import HDKey from 'hdkey';
+	const bip32 = BIP32Factory(ecc);
 	import { DOICHAIN } from '../lib/doichain.js'
 
+	let network = DOICHAIN
 	let theme = "g90";// "white" | "g10" | "g80" | "g90" | "g100"
 	let mnemonic = ''
 	let password = 'mnemonic'
-	let masterSeed = ''
-	let hdKey = ''
-	let derivationPath = ''
-	let ecc;
+	let root = ''
+	let xpriv
+	let xpub
+	let derivationPath = 'm/0/0/0'
+	let address;
 
-onMount(async () => {
-  ecc = (await import('tiny-secp256k1')).default;
-});</script>
+</script>
 
 <Theme bind:theme persist persistKey="__carbon-theme" />
 <h1>Welcome to Doichain Playground</h1>
@@ -34,44 +34,39 @@ onMount(async () => {
 		<Column><h2>2. Create a new HDKey from mnemonic</h2></Column>
 		<Column><TextInput labelText="Password" value={password} class="formElement" /></Column>
 		<Column><Button on:click={ async () => {
-			masterSeed = mnemonicToSeedSync(mnemonic, password ? password : "mnemonic").toString("hex")
-    	hdKey = HDKey.fromMasterSeed(Buffer.from(masterSeed, "hex"), DOICHAIN.bip32)
-		}} class="formElement" >Create HDKey from Mnemonic</Button></Column>
+			// masterSeed = mnemonicToSeedSync(mnemonic, password ? password : "mnemonic")
+    	// root = bip32.fromSeed(masterSeed)
+    	//  const mnemonic =
+      // 'praise you muffin lion enable neck grocery crumble super myself license ghost';
+			const seed = mnemonicToSeedSync(mnemonic);
+			root = bip32.fromSeed(seed,network);
+			xpriv = root.toBase58();
+			xpub = root.neutered().toBase58();
+			}} class="formElement" >Create HDKey from Mnemonic</Button></Column>
 
 		<Column>&nbsp;</Column>
 	</Row>
 	<Row>
-		<Column><h3>MasterSeed</h3></Column>
-		<Column><h4>{masterSeed}</h4></Column>
+		<Column><h3>xpriv</h3></Column>
+		<Column><h4>{xpriv || ''}</h4></Column>
 	</Row>
 	<Row>
-		<Column><h3>HD-PrivateKey</h3></Column>
-		<Column><h4>{hdKey?._privateKey?.toString('hex')  || ''}</h4></Column>
-	</Row>
-	<Row>
-		<Column><h3>HD-PublicKey</h3></Column>
-		<Column><h4>{hdKey?._publicKey?.toString('hex') || ''}</h4></Column>
-	</Row>
-	<Row>
-		<Column><h3>Public Extended Key:</h3></Column>
-		<Column><h4>{hdKey?.publicExtendedKey || ''}</h4></Column>
+		<Column><h3>xpub</h3></Column>
+		<Column><h4>{xpub || ''}</h4></Column>
 	</Row>
 	<Row>
 		<Column><h2>3. Create wallet from derivation path</h2></Column>
-		<Column><TextInput labelText="Derivation Path" value={derivationPath} class="formElement" /></Column>
+		<Column><TextInput labelText="Derivation Path" value={derivationPath}  class="formElement" /></Column>
 		<Column><Button on:click={ async () => {
-
-
-			const bip32 = BIP32Factory(ecc);
-
-			// const node = bip32.fromBase58(hdKey.publicExtendedKey);
-			// console.log("node",node)s
-			// const child =  node.derivePath('m/0/0');
-			// console.log("child",child)
-
+						const child =  root.derivePath(derivationPath);
+						address =  payments.p2pkh({ pubkey: child.publicKey, network }).address
 		}} class="formElement" >Create Wallet</Button></Column>
 
-		<Column>&nbsp;</Column>
+		<Column>&nbsp;&nbsp</Column>
+	</Row>
+	<Row>
+		<Column><h3>Address :</h3></Column>
+		<Column><h4>{address || ''}</h4></Column>
 	</Row>
 </Grid>
 
