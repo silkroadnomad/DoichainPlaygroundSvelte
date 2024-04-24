90fbfadf337577bb1ed880075f1f9e1686183d53
Nico Krause <post@nicokrause.com>
2024-04-24 09:28:07 +0500

    adding index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,6 @@
 // place files you want to import through the `$lib` alias in this folder.
+export { default as Doichain } from './doichain';
+export { default as ElectrumXClient } from './electrumx-client';
+export { default as getAddressTxs } from './getAddressTxs';
+export { default as getBalance } from './getBalance';
+export { default as IndexedDBUtil } from './indexedDBUtil';
