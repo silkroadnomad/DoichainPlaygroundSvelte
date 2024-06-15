0dd20c144ffa0289cc9293d8a6f246cdc10e043f
Nico Krause <post@nicokrause.com>
2024-06-15 21:13:02 +0300

    displaying found nameId, nameValue, doi value, txid

--- src/routes/nameCheck.svelte
+++ src/routes/nameCheck.svelte
@@ -2,18 +2,20 @@
 	import { Button, Column, Grid, Row, TextInput } from 'carbon-components-svelte';
 	import { crypto } from 'bitcoinjs-lib';
 	import { electrumClient } from './store.js';
+	import { processTransactionDetails } from '../lib/processTransactionDetail.js'
 
 	let nameToCheck = 'hello'
-	let result
+    let results = []; 
+
 	function pushData(data) {
 		let buffer = [];
 		const len = data.length;
 
-		if (len < 0x4c) { // for data length less than 76 bytes
+		if (len < 0x4c) { 
 			buffer.push(len);
-		} else if (len <= 0xff) { // for data length less than 256 bytes
+		} else if (len <= 0xff) { 
 			buffer.push(0x4c, len);
-		} else if (len <= 0xffff) { // for data length less than 65536 bytes
+		} else if (len <= 0xffff) { 
 			buffer.push(0x4d, len & 0xff, len >>> 8);
 		} else {
 			buffer.push(0x4e, len & 0xff, (len >>> 8) & 0xff, (len >>> 16) & 0xff, len >>> 24);
@@ -24,16 +26,20 @@
 	}
 
 	const checkName = async () => {
+		
 		let script = '53' + pushData(nameToCheck) + pushData(new Uint8Array([])) + '6d' + '75' + '6a';
 		let hash = crypto.sha256(Buffer.from(script, 'hex'));
 		let reversedHash = Buffer.from(hash.reverse()).toString("hex");
-
-		console.log("reversedHash", reversedHash);
-		result = await $electrumClient.request('blockchain.scripthash.get_history', [reversedHash]);
-		console.log("result", result);
+		results = []
+		const result = await $electrumClient.request('blockchain.scripthash.get_history', [reversedHash]);
+        for (const item of result) {
+            const detailResults = await processTransactionDetails($electrumClient,item.tx_hash);
+						results = [...results, ...detailResults.nameOpUtxos];
+        }
 	}
-
+	$:console.log("results", results);
 </script>
+
 <h1>Doichain Name Check</h1>
 <Grid>
 	<Row>
@@ -48,17 +54,28 @@
 	</Row>
 	<Row>
 		<Column>
-
+&nbsp;
 		</Column>
 	</Row>
-	{#if result && result.length > 0}
-	    <ul>
-	        {#each result as e}
-						Name found in Doichain TX: {e.tx_hash}
-	        {/each}
-	    </ul>
-	{:else}
-	    <p>No NameId found for given entry.</p>
-	{/if}
+    <Row>
+        <Column>
+            {#if results.length > 0}
+                <ul>
+                    {#each results as tx}
+											<p>Name found in Doichain TX
+												<li>txid: {tx.txid}</li>
+												<li>time: {tx.formattedBlocktime}</li>
+												<li>nameId: {tx.nameId}</li>
+												<li>nameValue: {tx.nameValue}</li>
+												<li>DOI amount:{tx.value}</li>
+											</p>
+
+                    {/each}
+                </ul>
+            {:else}
+                <p>No NameId found for given entry.</p>
+            {/if}
+        </Column>
+    </Row>
 </Grid>
 
