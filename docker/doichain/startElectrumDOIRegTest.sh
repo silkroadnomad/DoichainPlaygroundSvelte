3fcb6e26bc1e03306e63be57786c06870fe1e270
Nico Krause <post@nicokrause.com>
2024-12-02 19:10:58 +0500

    null

--- docker/doichain/startElectrumDOIRegTest.sh
+++ docker/doichain/startElectrumDOIRegTest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "starting doichain-regtest"
+echo "starting Doichain RegTest ElectrumX server"
 
 cd /electrumx/
 sudo mkdir -p certs
@@ -11,7 +11,7 @@ cd ..
 export COIN=Doichain
 export DB_DIRECTORY=/var/lib/electrumx
 export DAEMON_URL=http://admin:adminpw@regtest:18332
-export SERVICES=tcp://:50001,SSL://:50002,wss://:8443
+export SERVICES=tcp://:50001,SSL://:50002,wss://:8443,ws://localhost:8000
 export STRATUM_TCP_PORT=50001
 export STRATUM_TCP_SSL_PORT=50002
 export PEER_DISCOVERY=off
@@ -20,5 +20,6 @@ export SSL_CERTFILE=/electrumx/certs/server.crt
 export SSL_KEYFILE=/electrumx/certs/server.key
 export ALLOW_ROOT=non-empty
 export LOG_LEVEL=debug
+echo "starting electrumx server with services: $SERVICES"
 #sleep 100000
 ./electrumx_server
