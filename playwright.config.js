9236547d6aa7024f0b36aa0f65d3d1b3af4ede85
Nico Krause <post@nicokrause.com>
2024-12-04 15:36:32 +0500

    logs as list only

--- playwright.config.js
+++ playwright.config.js
@@ -2,11 +2,11 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  fullyParallel: true,
+  fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: 'list',
   use: {
     trace: 'on-first-retry',
     baseURL: 'http://localhost:4173',
